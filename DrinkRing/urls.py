66f528a23d255202a6ee85f6406c5d3af63095b6
Israel Ibinayin <eqa5@txstate.edu>
2024-05-15 16:47:02 -0500

    Setup Landing pages
    Setup Initial Game Settings and Other pages

--- DrinkRing/urls.py
+++ DrinkRing/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('', views.settings, name='settings'),
     path('', views.cards, name='cards'),
+    path('phase-one/', views.phase_one, name='phase_one'),
+    path('phase-two/', views.phase_two, name='phase_two'),
+    path('phase-three/', views.phase_three, name='phase_three'),
 ]
