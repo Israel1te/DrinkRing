7240dc8ef1f969f6ce17ca4e636144b3aedff4d7
Israel Ibinayin <eqa5@txstate.edu>
2024-05-23 00:48:07 -0500

    Added Register app and forms

--- DrinkRing/urls.py
+++ DrinkRing/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from DrinkGame import views
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', v.register, name='register'),
     path('', views.landing_page, name='landing_page'),
     path('', views.settings, name='settings'),
     path('', views.cards, name='cards'),
