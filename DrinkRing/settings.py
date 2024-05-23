a95a1139a6f35f734abcd850f0906eade450a6f1
Israel Ibinayin <eqa5@txstate.edu>
2024-05-23 01:34:30 -0500

    fixed userRegistration and forms

--- DrinkRing/settings.py
+++ DrinkRing/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DrinkGame',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'register',
 ]
 
@@ -119,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
