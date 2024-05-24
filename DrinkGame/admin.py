51f9bc622df48764041fdf7fe9e0aa173fcc3c13
Israel Ibinayin <eqa5@txstate.edu>
2024-05-24 04:59:26 -0500

    Set up sessions / player count and view functions to handle changes

--- DrinkGame/admin.py
+++ DrinkGame/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Card
+from .models import Session, Player
 
 admin.site.register(Card)
-# Register your models here.
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 1
+
+class SessionAdmin(admin.ModelAdmin):
+    inlines = [PlayerInline]
+
+admin.site.register(Session, SessionAdmin)
--- DrinkGame/views.py
+++ DrinkGame/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Session, Player
+from .forms import SessionForm, PlayerFormSet
+from django.contrib.auth.decorators import login_required
 from .models import Card
 
 def landing_page(request):
+    if request.method == 'POST':
+        existing_session = Session.objects.filter(user=request.user).first()
+        if existing_session:
+            return redirect('phase_one')
+        else:
+            session = Session.objects.create(user=request.user, num_players=0)
+            return redirect('phase_one')
     return render(request, 'landing_page.html')
+
+def update_players(request):
+    session = Session.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        new_num_players = int(request.POST.get('num_players'))
+        session.num_players = new_num_players
+        session.save()
+        return redirect('phase_2')
+    return render(request, 'phase_one_players.html')
 
 def settings(request):
     return render(request, 'settings.html')
@@ -12,6 +31,12 @@ def cards(request):
 
 def phase_one(request):
     template_name = 'phase_one_players.html'
+    session = Session.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        new_num_players = int(request.POST.get('num_players'))
+        session.num_players = new_num_players
+        session.save()
+        return redirect('phase_two')
     return render(request, template_name)
 
 def phase_two(request):
@@ -22,3 +47,4 @@ def phase_three(request):
     cards = Card.objects.all()
     template_name = 'phase_3_game.html'
     return render(request, template_name, {'cards':cards})
+
