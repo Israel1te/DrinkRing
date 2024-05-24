2ec643924aa33be59c5fe6ba7df8d996f71601e4
Israel Ibinayin <eqa5@txstate.edu>
2024-05-24 06:00:56 -0500

    Successfully stores player names and number in database

--- DrinkGame/views.py
+++ DrinkGame/views.py
@@ -4,6 +4,17 @@ from .forms import SessionForm, PlayerFormSet
 from django.contrib.auth.decorators import login_required
 from .models import Card
 
+def add_players(request):
+    session = Session.objects.filter(user=request.user).first()
+    player_range = range(1, session.num_players + 1)
+    players = session.players.all()
+    if request.method == 'POST':
+        for i in player_range:
+            name = request.POST.get(f'player_{i}')
+            Player.objects.create(session=session, name=name, points=0)
+        return redirect('phase_two')
+    return render(request, 'add_players.html', {'session': session, 'players': players, 'player_range': player_range})
+
 def landing_page(request):
     if request.method == 'POST':
         existing_session = Session.objects.filter(user=request.user).first()
@@ -20,7 +31,7 @@ def update_players(request):
         new_num_players = int(request.POST.get('num_players'))
         session.num_players = new_num_players
         session.save()
-        return redirect('phase_2')
+        return redirect('add_players')
     return render(request, 'phase_one_players.html')
 
 def settings(request):
@@ -36,7 +47,7 @@ def phase_one(request):
         new_num_players = int(request.POST.get('num_players'))
         session.num_players = new_num_players
         session.save()
-        return redirect('phase_two')
+        return redirect('add_players')
     return render(request, template_name)
 
 def phase_two(request):
