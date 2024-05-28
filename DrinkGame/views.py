8450fdf018e761b3ec4113e0ef77937dac4d7ed6
Israel Ibinayin <eqa5@txstate.edu>
2024-05-28 01:56:46 -0500

    updated views, and started phase 3

--- DrinkGame/views.py
+++ DrinkGame/views.py
@@ -14,6 +14,7 @@ def add_players(request):
         for i in player_range:
             name = request.POST.get(f'player_{i}')
             Player.objects.create(session=session, name=name, points=0)
+        players = session.players.all()
         return redirect('phase_two')
     return render(request, 'add_players.html', {'session': session, 'players': players, 'player_range': player_range})
 
@@ -56,8 +57,10 @@ def phase_two(request):
     template_name = 'phase_two_mode.html'
     return render(request, template_name)
 
-def phase_three(request):
+def phase_three_quick(request):
     cards = Card.objects.all()
-    template_name = 'phase_3_game.html'
-    return render(request, template_name, {'cards':cards})
+    session = Session.objects.filter(user=request.user).first()
+    players = session.players.all()
+    template_name = 'phase_3_quick.html'
+    return render(request, template_name, {'cards':cards, 'session': session, 'players': players})
 
